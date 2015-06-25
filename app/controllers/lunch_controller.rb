69c3a89f0a1fabc55c1f03e6733fa839a1a708cd
Ivan Kozakov <ivanko@interlink-ua.com>
2015-06-25 11:52:15 +0300

    update UI and routes

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,20 @@
 class LunchController < ApplicationController
 
   def show
+  end
 
+  def feedback
+    time = Time.now
+    LunchUserResponse.create(year: time.year,
+       month: time.month, day: time.day,
+       vote: params[:vote], user_name: params[:user_name], comment: params[:comment])
+    redirect_to action: 'thanks_for_response'#, :params => { revision_name: rn, name: flow}
+  end
+
+  def thanks_for_response
+  end
+
+  def votes_list
+    @votes = LunchUserResponse.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lunch#show'
+
+  post '/feedback' => 'lunch#feedback'
+  get '/lunch/votes_list' => 'lunch#votes_list'
+  get '/lunch/thanks_for_response' => 'lunch#thanks_for_response'
 end
